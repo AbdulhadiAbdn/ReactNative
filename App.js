5365002e3aa0f2fcbe29203a1d7b4075d40550b7
Abdulhadi Abdn <abdulhadiabdn@gmail.com>
2024-03-28 02:54:21 +0530

    refactoring

--- App.js
+++ App.js
@@ -15,17 +15,15 @@ export default function App() {
   const [user, setUser] = useState();
   const [isReady, setIsReady] = useState(false);
 
-  const restoreToken = async () => {
-    const token = await authStorage.getToken();
-    if (!token) return;
-
-    setUser(jwtDecode(token));
+  const restoreUser = async () => {
+    const user = await authStorage.getUser();
+    if (user) setUser(jwtDecode(user));
   };
 
   if (!isReady)
     return (
       <AppLoading
-        startAsync={restoreToken}
+        startAsync={restoreUser}
         onFinish={() => setIsReady(true)}
         onError={(error) => console.log(error)}
       />
