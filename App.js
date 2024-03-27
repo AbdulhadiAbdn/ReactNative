ef49ae4908ef4a78b588a4239f5895dfe6d197fe
Abdulhadi Abdn <abdulhadiabdn@gmail.com>
2024-03-28 02:47:45 +0530

    edit the splash screen

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
+import "core-js/stable/atob";
+import AppLoading from "expo-app-loading";
+
 import theme from "./App/config/theme";
 import AppNavigator from "./App/navigation/AppNavigator";
 import OfflineNotice from "./App/components/OfflineNotice";
 import AuthNavigator from "./App/navigation/authNavigator";
-import { useEffect, useState } from "react";
 import AuthContext from "./App/auth/context";
 import authStorage from "./App/auth/storage";
-import { jwtDecode } from "jwt-decode";
-import "core-js/stable/atob";
 
 export default function App() {
   const [user, setUser] = useState();
+  const [isReady, setIsReady] = useState(false);
 
   const restoreToken = async () => {
     const token = await authStorage.getToken();
@@ -19,9 +22,14 @@ export default function App() {
     setUser(jwtDecode(token));
   };
 
-  useEffect(() => {
-    restoreToken();
-  }, []);
+  if (!isReady)
+    return (
+      <AppLoading
+        startAsync={restoreToken}
+        onFinish={() => setIsReady(true)}
+        onError={(error) => console.log(error)}
+      />
+    );
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
