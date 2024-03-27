3840b2962275f7ed3ce8a8cd397da460b31abcda
Abdulhadi Abdn <abdulhadiabdn@gmail.com>
2024-03-27 18:19:54 +0530

    set up authentication in login page

--- App.js
+++ App.js
@@ -2,14 +2,18 @@ import { NavigationContainer } from "@react-navigation/native";
 import theme from "./App/config/theme";
 import AppNavigator from "./App/navigation/AppNavigator";
 import OfflineNotice from "./App/components/OfflineNotice";
+import AuthNavigator from "./App/navigation/authNavigator";
+import { useState } from "react";
+import AuthContext from "./App/auth/context";
 
 export default function App() {
+  const [user, setUser] = useState();
   return (
-    <>
+    <AuthContext.Provider value={{ user, setUser }}>
       <OfflineNotice />
       <NavigationContainer theme={theme}>
-        <AppNavigator />
+        {user ? <AppNavigator /> : <AuthNavigator />}
       </NavigationContainer>
-    </>
+    </AuthContext.Provider>
   );
 }
--- App/screens/LoginScreen.js
+++ App/screens/LoginScreen.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Image, StyleSheet } from "react-native";
 import * as Yup from "yup";
+import { jwtDecode } from "jwt-decode";
+import "core-js/stable/atob";
+
+import apiAuth from "../api/auth";
 import Screen from "../components/Screen";
-import { AppForm, AppFormFeild, SubmitButton } from "../components/forms";
+import {
+  ErrorMessage,
+  AppForm,
+  AppFormFeild,
+  SubmitButton,
+} from "../components/forms";
+import AuthContext from "../auth/context";
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label("Email"),
@@ -10,14 +20,29 @@ const validationSchema = Yup.object().shape({
 });
 
 function LoginScreen() {
+  const authContext = useContext(AuthContext);
+  const [loginFaild, setLoginFaild] = useState(false);
+
+  const handleSubmit = async ({ email, password }) => {
+    const result = await apiAuth.login(email, password);
+    if (!result.ok) return setLoginFaild(true);
+
+    setLoginFaild(false);
+    const user = jwtDecode(result.data);
+    authContext.setUser(user);
+  };
   return (
     <Screen style={styles.container}>
       <Image source={require("../assets/logo-red.png")} style={styles.logo} />
       <AppForm
         initialValues={{ email: "", password: "" }}
-        onSubmit={(values) => console.log(values)}
+        onSubmit={handleSubmit}
         validationSchema={validationSchema}
       >
+        <ErrorMessage
+          error="Invalid email and/or password"
+          visible={loginFaild}
+        />
         <AppFormFeild
           name="email"
           label="Email"
